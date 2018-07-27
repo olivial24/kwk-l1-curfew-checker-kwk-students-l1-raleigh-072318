be80f16861215af3a9466a4644990bc7c1b1ce37
olivial24 <olivia.lorenz24@gmail.com>
2018-07-27 14:52:03 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -6,7 +6,7 @@ end
 
 def curfew_checker(time)
  if time == 11 
-   "You're in trouble! Better get back to Hogwarts quick!"
+   "You're in trouble! Better get home quick!"
    elsif time < 11 
    "You have 2 hour(s) left to keep having fun!"
  end 
